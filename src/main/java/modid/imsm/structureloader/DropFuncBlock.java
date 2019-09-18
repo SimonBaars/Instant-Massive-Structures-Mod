346dbfe9a4adafcb30bc2d17354ddb5894e4e2b1
Simon <simon.mailadres@gmail.com>
2019-09-18 06:32:31 +0200

    DropFuncBlock

--- src/main/java/modid/imsm/structureloader/DropFuncBlock.java
+++ src/main/java/modid/imsm/structureloader/DropFuncBlock.java
@@ -1,14 +1,11 @@
 package modid.imsm.structureloader;
 
-import modid.imsm.core.IMSM;
-import net.minecraft.block.BlockColored;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 
 public class DropFuncBlock
 {
@@ -20,19 +17,26 @@ public class DropFuncBlock
 
 	private static boolean setBlock(World world, IBlockState state, BlockPos pos, NBTTagCompound tileEntity, boolean update, boolean isLive)
 	{
-		try{
-		Chunk chunk = world.getChunkFromBlockCoords(pos);
+		//try{
+		Chunk chunk = world.getChunk(pos);
+		if (world.getBlockState(pos) != state)
+            world.setBlockState(pos, state, 2);
+
+        if (update && world instanceof World)
+            ((World) world).markAndNotifyBlock(pos, ((World) world).getChunk(pos),
+                world.getBlockState(pos), state, 3);
+
 		//Chunk chunkClient = Minecraft.getInstance().world.getChunkFromBlockCoords(pos);
-		ExtendedBlockStorage storageArray = chunk.getBlockStorageArray()[pos.getY() >> 4];
+		//ExtendedBlockStorage storageArray = chunk.getBlockStorageArray()[pos.getY() >> 4];
 		//System.out.println(pos.getX()+", "+pos.getY()+", "+pos.getZ());
-		if (storageArray == null) storageArray = chunk.getBlockStorageArray()[pos.getY() >> 4] = new ExtendedBlockStorage(pos.getY() >> 4 << 4, !world.provider.getHasNoSky());
+		//if (storageArray == null) storageArray = chunk.getBlockStorageArray()[pos.getY() >> 4] = new ExtendedBlockStorage(pos.getY() >> 4 << 4, !world.provider.getHasNoSky());
 		
-		if (storageArray.get(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15).getBlock() != state.getBlock() || state.getBlock() instanceof BlockColored)
-		{
-			IBlockState oldState = world.getBlockState(pos);
+		//if (storageArray.get(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15).getBlock() != state.getBlock() || state.getBlock() instanceof BlockColored)
+		//{
+		//	IBlockState oldState = world.getBlockState(pos);
 			//storageArray.set(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15, state);
 			//chunkClient.getBlockStorageArray()[pos.getY() >> 4].set(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15, state);
-			storageArray.set(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15, state);
+		//	storageArray.set(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15, state);
 			
 			//world.setBlockState(pos, state);
 			//int timesFaster=1;
@@ -53,7 +57,7 @@ public class DropFuncBlock
 			//if(world.isRemote){
 			//	IMSM.eventHandler.lightUpdate.addClientProcess(pos);
 			//} else {
-				IMSM.eventHandler.lightUpdate.addServerProcess(pos);
+			//	IMSM.eventHandler.lightUpdate.addServerProcess(pos);
 				//IMSM.eventHandler.lightUpdate.addClientProcess(pos);
 			//}
 			/*if(world.isDaytime()){
@@ -67,9 +71,9 @@ world.checkLight(pos);
 			}
 			}*/
 				//world.setBlockState(pos, oldState)
-				chunk.setChunkModified();
+			//	chunk.setChunkModified();
 			//chunkClient.setChunkModified();
-			world.markBlockRangeForRenderUpdate(pos,pos);//TODO: Stil a hack
+			//world.markBlockRangeForRenderUpdate(pos,pos);//TODO: Stil a hack
 			//Minecraft.getInstance().world.markBlockRangeForRenderUpdate(pos,pos);//TODO: Stil a hack
 			/*if(world.isRemote){
 			//world.checkLight(pos);
@@ -77,9 +81,9 @@ world.checkLight(pos);
 				world.setLightFor(EnumSkyBlock.BLOCK, pos, 15);
 			}*/
 			//world.setLightFor(EnumSkyBlock.BLOCK, pos, 15);
-			/*if (update || state.getBlock()!=Blocks.air)*/ world.markAndNotifyBlock(pos, chunk, state, oldState, 3);
+			/*if (update || state.getBlock()!=Blocks.air)*/ //world.markAndNotifyBlock(pos, chunk, state, oldState, 3);
 			//Minecraft.getInstance().world.markAndNotifyBlock(pos, chunk, state, oldState, 3);
-		}
+		//}
 
 		if (tileEntity != null && state.getBlock().hasTileEntity(state) && !isLive)
 		{
@@ -87,22 +91,30 @@ world.checkLight(pos);
 			BlockPos chunkPos = new BlockPos(pos.getX() & 15, pos.getY(), pos.getZ() & 15);
 			TileEntity blockTileEntity = chunk.getTileEntity(chunkPos, Chunk.EnumCreateEntityType.CHECK);
 
-			blockTileEntity = state.getBlock().createTileEntity(world, state);
-			blockTileEntity.readFromNBT(tileEntity);
+			blockTileEntity = state.getBlock().createTileEntity(state, world);
+			blockTileEntity.read(tileEntity);
 			world.setTileEntity(pos, blockTileEntity);
 			blockTileEntity.updateContainingBlockInfo();
 		}
-		} catch (Exception e){
-			return false;
-		}
+		//} catch (Exception e){
+		//	return false;
+		//}
 		return true;
 	}
 
-	public static synchronized void setTileEntity(World world, IBlockState state, BlockPos pos, NBTTagCompound tileEntity)
+	public static synchronized void setTileEntity(World world, IBlockState state, BlockPos pos, NBTTagCompound nbt)
 	{
-		if (tileEntity != null && state.getBlock().hasTileEntity(state))
+		TileEntity tileEntity = world.getTileEntity(pos);
+        if (tileEntity != null) {
+            tileEntity.handleUpdateTag(nbt);
+            if (world instanceof World)
+                ((World) world).setTileEntity(pos, tileEntity);
+        } else {
+            System.out.println("Error setting tile entity for block: " + state);
+        }
+		/*if (tileEntity != null && state.getBlock().hasTileEntity(state))
 		{
-			Chunk chunk = world.getChunkFromBlockCoords(pos);
+			Chunk chunk = world.getChunk(pos);
 			ExtendedBlockStorage storageArray = chunk.getBlockStorageArray()[pos.getY() >> 4];
 			if (storageArray == null) storageArray = chunk.getBlockStorageArray()[pos.getY() >> 4] = new ExtendedBlockStorage(pos.getY() >> 4 << 4, !world.provider.getHasNoSky());
 
@@ -110,10 +122,10 @@ world.checkLight(pos);
 			BlockPos chunkPos = new BlockPos(pos.getX() & 15, pos.getY(), pos.getZ() & 15);
 			TileEntity blockTileEntity = chunk.getTileEntity(chunkPos, Chunk.EnumCreateEntityType.CHECK);
 
-			blockTileEntity = state.getBlock().createTileEntity(world, state);
-			blockTileEntity.readFromNBT(tileEntity);
+			blockTileEntity = state.getBlock().createTileEntity(state, world);
+			blockTileEntity.read(tileEntity);
 			world.setTileEntity(pos, blockTileEntity);
 			blockTileEntity.updateContainingBlockInfo();
-		}
+		}*/
 	}
 }
